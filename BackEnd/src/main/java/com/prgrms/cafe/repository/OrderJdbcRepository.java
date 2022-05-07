2d8d827d84e685dcf686cdaf306bc32a94551a97
HyoungUkJJang <guddnr1451@gmail.com>
2022-05-07 12:35:44 +0900

    refactor : OrderRepository 기능 수정하기

--- BackEnd/src/main/java/com/prgrms/cafe/repository/OrderJdbcRepository.java
+++ BackEnd/src/main/java/com/prgrms/cafe/repository/OrderJdbcRepository.java
@@ -1,11 +1,20 @@
 package com.prgrms.cafe.repository;
 
+import static com.prgrms.cafe.repository.JdbcUtils.toLocalDateTime;
+import static com.prgrms.cafe.repository.JdbcUtils.toUUID;
+
+import com.prgrms.cafe.model.Email;
 import com.prgrms.cafe.model.Order;
-import com.prgrms.cafe.model.OrderItem;
+import com.prgrms.cafe.model.OrderStatus;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -21,24 +30,79 @@ public class OrderJdbcRepository implements OrderRepository {
 
     @Override
     public Order save(Order order) {
-        jdbcTemplate.update(
+        int update = jdbcTemplate.update(
             "insert into orders(order_id, email, address, postcode, order_status, created_at, updated_at)"
-                + " values(UUID_TO_BIN(:orderId), :email, :address, :postcode, :orderStatus, :createdAt, :updatedAt)"
-            ,toOrderParamMap(order));
-
-        order.getOrderItems().forEach(
-            item -> {
-                jdbcTemplate.update(
-                    "insert into order_items(order_id, product_id, category, price, quantity, created_at, updated_at)"
-                        + " values(UUID_TO_BIN(:orderId), UUID_TO_BIN(:productId), :category, :price, :quantity, :createdAt, :updatedAt)",
-                    toOrderItemParamMap(order.getOrderId(), order.getCreatedAt(), order.getUpdatedAt(), item));
-            }
-        );
+                + " values(UUID_TO_BIN(:orderId), :email, :address, :postcode, :orderStatus, :createdAt, :updatedAt)",
+            toOrderParamMap(order));
+        if (update == 0) {
+            throw new RuntimeException("저장이 되지 않았습니다.");
+        }
 
         return order;
     }
 
-    private Map<String,Object> toOrderParamMap(Order order) {
+    @Override
+    public void update(UUID orderId, String address, String postCode) {
+        jdbcTemplate.update(
+            "update orders set address = :address, postcode = :postcode, updated_at = :updatedAt"
+                + " where order_id = UUID_TO_BIN(:orderId)",
+            new HashMap<String, Object>() {{
+                put("orderId", orderId.toString().getBytes());
+                put("address", address);
+                put("postcode", postCode);
+                put("updatedAt", LocalDateTime.now());
+            }}
+        );
+    }
+
+    @Override
+    public void updateStatus(UUID orderId, String orderStatus) {
+        int update = jdbcTemplate.update(
+            "update orders set order_status = :orderStatus"
+                + " where order_id = UUID_TO_BIN(:orderId)",
+            new HashMap<String, Object>() {{
+                put("orderId", orderId.toString().getBytes());
+                put("orderStatus", orderStatus);
+            }});
+        if (update == 0) {
+            throw new RuntimeException("수정이 되지 않았습니다.");
+        }
+    }
+
+    @Override
+    public List<Order> findAll() {
+        return jdbcTemplate.query("select * from orders", orderRowMapper);
+    }
+
+    @Override
+    public Optional<Order> findById(UUID orderId) {
+        try {
+            Optional<Order> order = Optional.ofNullable(
+                jdbcTemplate.queryForObject(
+                    "select * from orders where order_id = UUID_TO_BIN(:orderId)"
+                    , Collections.singletonMap("orderId", orderId.toString().getBytes()),
+                    orderRowMapper));
+            return order;
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    private static final RowMapper<Order> orderRowMapper = (resultSet, i) -> {
+        UUID orderId = toUUID(resultSet.getBytes("order_id"));
+        Email emil = new Email(resultSet.getString("email"));
+        String address = resultSet.getString("address");
+        String postcode = resultSet.getString("postcode");
+        OrderStatus orderStatus = OrderStatus.valueOf(resultSet.getString("order_status"));
+        LocalDateTime createdAt = toLocalDateTime(resultSet.getTimestamp("created_at"));
+        LocalDateTime updatedAt = toLocalDateTime(resultSet.getTimestamp("updated_at"));
+        return new Order(
+            orderId, emil, address, postcode, Collections.emptyList(), orderStatus, createdAt,
+            updatedAt
+        );
+    };
+
+    private Map<String, Object> toOrderParamMap(Order order) {
         Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("orderId", order.getOrderId().toString().getBytes());
         paramMap.put("email", order.getEmail().getAddress());
@@ -47,19 +111,6 @@ public class OrderJdbcRepository implements OrderRepository {
         paramMap.put("orderStatus", order.getOrderStatus().toString());
         paramMap.put("createdAt", order.getCreatedAt());
         paramMap.put("updatedAt", order.getUpdatedAt());
-        return paramMap;
-    }
-
-    private Map<String, Object> toOrderItemParamMap(UUID orderId, LocalDateTime createdAt,
-        LocalDateTime updatedAt, OrderItem item) {
-        HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("orderId", orderId.toString().getBytes());
-        paramMap.put("productId", item.productId().toString().getBytes());
-        paramMap.put("category",item.category().toString());
-        paramMap.put("price", item.price());
-        paramMap.put("quantity", item.quantity());
-        paramMap.put("createdAt", createdAt);
-        paramMap.put("updatedAt", updatedAt);
         return paramMap;
     }
 
--- BackEnd/src/main/java/com/prgrms/cafe/repository/OrderRepository.java
+++ BackEnd/src/main/java/com/prgrms/cafe/repository/OrderRepository.java
@@ -1,9 +1,20 @@
 package com.prgrms.cafe.repository;
 
 import com.prgrms.cafe.model.Order;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface OrderRepository {
 
     Order save(Order order);
+
+    void update(UUID orderId, String address, String postcde);
+
+    void updateStatus(UUID orderId, String orderStatus);
+
+    List<Order> findAll();
+
+    Optional<Order> findById(UUID orderId);
 
 }
