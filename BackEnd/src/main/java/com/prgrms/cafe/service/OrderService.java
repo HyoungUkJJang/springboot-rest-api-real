564e50a977e2ba8fce9904e61df94b16de23d1e6
HyoungUkJJang <guddnr1451@gmail.com>
2022-05-07 12:40:01 +0900

    refactor : Order 서비스 기능 추가 및 수정하기

--- BackEnd/src/main/java/com/prgrms/cafe/service/OrderService.java
+++ BackEnd/src/main/java/com/prgrms/cafe/service/OrderService.java
@@ -4,9 +4,18 @@ import com.prgrms.cafe.model.Email;
 import com.prgrms.cafe.model.Order;
 import com.prgrms.cafe.model.OrderItem;
 import java.util.List;
+import java.util.UUID;
 
 public interface OrderService {
 
     Order createOrder(Email email, String address, String postcode, List<OrderItem> orderItems);
+
+    List<Order> getOrders();
+
+    void changeStatus(UUID orderId, String orderStatus);
+
+    void changeAddressAndPostcode(UUID orderId, String address, String postcode);
+
+    Order getOrder(UUID orderId);
 
 }
--- BackEnd/src/main/java/com/prgrms/cafe/service/OrderServiceImpl.java
+++ BackEnd/src/main/java/com/prgrms/cafe/service/OrderServiceImpl.java
@@ -4,25 +4,64 @@ import com.prgrms.cafe.model.Email;
 import com.prgrms.cafe.model.Order;
 import com.prgrms.cafe.model.OrderItem;
 import com.prgrms.cafe.model.OrderStatus;
+import com.prgrms.cafe.repository.OrderItemRepository;
 import com.prgrms.cafe.repository.OrderRepository;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    public OrderServiceImpl(OrderRepository orderRepository,
+        OrderItemRepository orderItemRepository) {
         this.orderRepository = orderRepository;
+        this.orderItemRepository = orderItemRepository;
     }
 
     @Override
-    public Order createOrder(Email email, String address, String postcode, List<OrderItem> orderItems) {
-        return orderRepository.save(
-            new Order(
-                UUID.randomUUID(), email, address, postcode, orderItems, OrderStatus.ACCEPTED)
-        );
+    @Transactional
+    public Order createOrder(Email email, String address, String postcode,
+        List<OrderItem> orderItems) {
+        Order order = orderRepository.save(
+            new Order(UUID.randomUUID(), email, address, postcode, orderItems,
+                OrderStatus.ACCEPTED));
+        orderItems.forEach(item -> orderItemRepository.save(item));
+
+        return order;
     }
+
+    @Override
+    public List<Order> getOrders() {
+        return orderRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void changeStatus(UUID orderId, String orderStatus) {
+        orderRepository.updateStatus(orderId, orderStatus);
+    }
+
+    @Override
+    @Transactional
+    public void changeAddressAndPostcode(UUID orderId, String address, String postcode) {
+        orderRepository.update(orderId, address, postcode);
+    }
+
+    @Override
+    public Order getOrder(UUID orderId) {
+        List<OrderItem> orderItems = orderItemRepository.findById(orderId);
+        Order order = orderRepository.findById(orderId)
+            .orElseThrow(NoSuchElementException::new);
+
+        return new Order(order.getOrderId(), order.getEmail(), order.getAddress(), order.getPostcode(),
+            orderItems, order.getOrderStatus(), order.getCreatedAt(), order.getUpdatedAt());
+    }
+
 }
